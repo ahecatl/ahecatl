df5b4cb1efddf06b6af896e0bf203204abf9e487
Arnaud Bellec <arnaud.bellec@gmail.com>
2015-04-06 18:14:50 +0200

    Yay infinite background

---
+++ Assets/Scripts/RepeatBackground.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RepeatBackground : MonoBehaviour 
+{ 
+	public float increment;
+
+	void OnBecameInvisible()
+	{
+		//calculate current position
+		Vector3 position = gameObject.transform.position;
+		//calculate new position
+		float newX = position.x + increment;
+		//move to new position when invisible
+		gameObject.transform.position = new Vector3 (newX, position.y, 0f);
+	}
+}
