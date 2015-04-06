011e29fb992a7a946f503324184d625d7f3fd5d7
Arnaud Bellec <arnaud.bellec@gmail.com>
2015-04-06 18:51:25 +0200

    Parallax background scroll

---
+++ Assets/Scripts/ParallaxBackground.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParallaxBackground : MonoBehaviour {
+	public float scrollFactor;
+	public Transform trackingCamera;
+
+	private Vector3 m_lastTargetPosition;
+
+	private void Start() {
+		m_lastTargetPosition = trackingCamera.position;
+	}
+
+	private void Update() {
+		float deltaX = trackingCamera.position.x - m_lastTargetPosition.x;
+		m_lastTargetPosition = trackingCamera.position;
+
+		Vector3 position = transform.position;
+		transform.position = new Vector3 (position.x + deltaX * scrollFactor, position.y, position.z);
+	}
+}
